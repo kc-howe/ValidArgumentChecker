2489b5905363afe6fc94d490f8e3fe71a8dabe5a
Kenneth Howe <51336023+kc-howe@users.noreply.github.com>
2021-03-16 21:30:17 -0400

    Added quotient syntax.
    Premises can now be separated by commas, grouped with brackets, and deductively concluded with a quotient.

--- ValidArgumentChecker.cpp
+++ ValidArgumentChecker.cpp
@@ -23,6 +23,16 @@ public:
 	}
 } pex;
 
+// Throw if mismatched brackets
+class braexception : public exception
+{
+public:
+	virtual const char* what() const throw()
+	{
+		return "Mismatched brackets.";
+	}
+} bex;
+
 // Throw if invalid syntax
 class synexception : public exception
 {
@@ -125,6 +135,16 @@ public:
 				throw sex;
 			}
 			return ((this->leftArg)->evaluate(atomValues) && (this->rightArg)->evaluate(atomValues)) || !((this->leftArg)->evaluate(atomValues) || (this->rightArg)->evaluate(atomValues));
+		case ',':
+			if (!this->leftArg || !this->rightArg) {
+				throw sex;
+			}
+			return (this->leftArg)->evaluate(atomValues) && (this->rightArg)->evaluate(atomValues);
+		case '/':
+			if (!this->leftArg || !this->rightArg) {
+				throw sex;
+			}
+			return !(this->leftArg)->evaluate(atomValues) || (this->rightArg)->evaluate(atomValues);
 		case '0':
 			return false;
 		case '1':
@@ -156,21 +176,49 @@ public:
 	}
 };
 
+// Checks if statement is atomic
+bool isAtomic(char c) {
+	return isalpha(c) || c == '0' || c == '1';
+}
 
 // Just returns a map of op precedences
 map<char, int>* getOperatorPrecedence() {
 	static map<char, int> p;
-	p['~'] = 5;
-	p['&'] = 4;
-	p['|'] = 3;
-	p['>'] = 2;
-	p['='] = 1;
+	p['~'] = 7;
+	p['&'] = 6;
+	p['|'] = 5;
+	p['>'] = 4;
+	p['='] = 3;
+	p[','] = 2;
+	p['/'] = 1;
 
 	return &p;
 }
 
 // Converts infix expressions to postfix
 queue<char>* polishParse(string inputSentence) {
+
+	// Quick syntax check, looking for duplicate/misplaced '{' or '/' symbols
+	int quotientIndex = -1;
+	int bracketIndex = -1;
+	for (int i = 0; i < inputSentence.length(); i++) {
+		if (inputSentence[i] == '/') {
+			if (quotientIndex > -1) {
+				throw sex;
+			}
+			else {
+				quotientIndex = i;
+			}
+		}
+		else if (inputSentence[i] == '{') {
+			if (bracketIndex > -1 || bracketIndex > quotientIndex) {
+				throw sex;
+			}
+			else {
+				bracketIndex = i;
+			}
+		}
+	}
 
 	// Using Shunting-Yard for the conversion
 
@@ -190,31 +238,26 @@ queue<char>* polishParse(string inputSentence) {
 
 		switch (token) {
 
-		// Push unary operator ~ to operator stack
+			// Push unary operator ~ to operator stack
 		case '~':
 			operators.push(token);
 			break;
 
-		// Push tautology and contradiction atoms to output
-		case '0':
-			output.push(token);
-			break;
-
-		case '1':
-			output.push(token);
-			break;
-
-		// If token is left paren, push to stack
+			// If token is left paren, push to stack
 		case '(':
 			operators.push(token);
 			break;
 
-		// If token is right paren, push operators until left paren is reached
+			// If token is right paren, push operators until left paren is reached
 		case ')':
 			while (!operators.empty() && operators.top() != '(') {
+				if (operators.top() == ',') {
+					throw sex;
+				}
 				output.push(operators.top());
 				operators.pop();
 			}
+
 			// CHECK FOR MISMATCHED PARANTHESES
 			// (i.e. operators run out before left paren reached)
 			if (operators.empty() || operators.top() != '(') {
@@ -223,13 +266,37 @@ queue<char>* polishParse(string inputSentence) {
 			operators.pop();
 			break;
 
-		// If white space, ignore
+			// Similar approach for left and right brackets
+		case '{':
+			operators.push(token);
+			break;
+
+		case '}':
+			while (!operators.empty() && operators.top() != '{') {
+				if (operators.top() == '}') {
+					throw sex;
+				}
+				output.push(operators.top());
+				operators.pop();
+			}
+
+			// CHECK FOR MISMATCHED BRACKETS
+			if (operators.empty() || operators.top() != '{') {
+				throw bex;
+			}
+			operators.pop();
+			break;
+
+			// If space, ignore
 		case ' ':
 			break;
 
 		default:
-			// If token is alpha, push it to output queue
-			if (isalpha(token)) {
+			// If token is atomic, push it to output queue
+			if (isAtomic(token)) {
+				if (i < inputSentence.length() - 1 && isAtomic(inputSentence[i + 1])) {
+					throw sex;
+				}
 				output.push(token);
 			}
 
@@ -278,7 +345,7 @@ Sentence* constructSentence(queue<char>* polish) {
 	while (!polish->empty()) {
 
 		// If next token is atomic, push to stack
-		if (isalpha(polish->front()) || polish->front() == '0' || polish->front() == '1') {
+		if (isAtomic(polish->front())) {
 			Sentence* atom = new Sentence(polish->front());
 			components.push(atom);
 			polish->pop();
@@ -457,12 +524,15 @@ int main() {
 				" a-Z - propositions\n" <<
 				"  0  - contradiction\n" <<
 				"  1  - tautology\n" <<
+				" { } - brackets (optional, group premises)\n" <<
 				" ( ) - parentheses\n" <<
-				"  ~  - not\n" << 
-				"  &  - and\n" << 
-				"  |  - or\n" << 
-				"  >  - if-then\n" << 
-				"  =  - if and only if\n" << endl;
+				"  ~  - not\n" <<
+				"  &  - and\n" <<
+				"  |  - or\n" <<
+				"  >  - if-then\n" <<
+				"  =  - if and only if\n" <<
+				"  ,  - separator\n" <<
+				"  /  - quotient (deductive transition)\n" << endl;
 			cout << "COMMANDS:\n" <<
 				"help         Prints this page.\n" <<
 				"show-table   Prints an abbreviated truth table for the previous argument.\n" <<
@@ -477,10 +547,10 @@ int main() {
 				"Premise 1:  Either it is cloudy outside, or it is not raining.\n" <<
 				"Premise 2:  It is raining.\n" <<
 				"Conclusion: Therefore, it is cloudy outside.\n\n" <<
-				"Using \'&\' to connect our premises and \'>\' to imply our conclusion, this argument can be written:\n\n" <<
-				"((C | ~R) & R) > C\n\n" <<
+				"Using \',\' to separate our premises, '{ }' to group them, and \'/\' to draw our conclusion, this argument can be written:\n\n" <<
+				"{C | ~R, R} / C\n\n" <<
 				"We can also use the '=' operator to validate the logical equivalence of two expressions:\n\n" <<
-				"(~P | Q) = (P > Q)\n" << 
+				"(~P | Q) = (P > Q)\n" <<
 				"(P & ~P) = 0\n" << endl;
 		}
 		else if (argument == "quit") {
